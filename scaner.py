8cde1c875d09f6dbfd09f27ce1501e48fcf3a657
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-10-06 20:11:06 -0600

    Update scaner.py

--- scaner.py
+++ scaner.py
@@ -1,1 +1,12 @@
+from tipoToken import tipoToken
+from Token import Token
+import interprete as scan
+import os,sys
 
+os.system('clear')
+
+if len(sys.argv)>1 and len(sys.argv)<3:
+	if os.path.exists(sys.argv[1]):
+		#print('\"Scaneando\" el archivo '+sys.argv[1])
+		archivo=open(sys.argv[1],'r')
+		cont=archivo.read()
