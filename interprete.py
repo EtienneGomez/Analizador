b8ba5cabbc59111c21c7814f5dbf87504ff45777
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-10-08 17:12:07 -0600

    Update interprete.py
    No estaba bien tabulado

--- interprete.py
+++ interprete.py
@@ -7,20 +7,30 @@ estructura=Token('','',None)
 
 #Para identificar una cadena
 def identificador(cad):
-  j=1
-  if cad[0].isalpha():
-    for i in cad:
-      if i.isdigit() or i.isalpha() or i=='_' or i=='-':
-        j+=1
-      else:
-        break
-      print(Token(tipoToken.IDENTIFIER,cad[:j-1],None).toString())
-      estructura.almacenar(tipoToken.IDENTIFIER,cad[:j-1],None)
-      return j-1
+	j=1
+	if cad[0].isalpha():
+		for i in cad:
+			if i.isdigit() or i.isalpha() or i=='_' or i=='-':
+				j+=1
+			else:
+				break
+		print(Token(tipoToken.IDENTIFIER,cad[:j-1],None).toString())
+		estructura.almacenar(tipoToken.IDENTIFIER,cad[:j-1],None)
+		return j-1
 
 #Funcion para manipular el texto desde un archivo 
-def scanerArchivo():
-  print('pa\' los archivos')
+def scanerArchivo(cont):
+	j=0
+	bandera=0
+	for i in range(0,len(cont)):
+		if j>=len(cont):
+			break
+		else:
+			bandera=identificar(cont[j:])
+		if bandera>1:
+			j+=bandera
+		else:
+			j+=1
 
 #Funcion que analice el texto
 def lineaComando():
