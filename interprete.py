605cf611070a00b27d2d7b8c4e1694a7467e427b
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-10-08 16:57:59 -0600

    Update interprete.py
    Regresar tokens e identificar cadenas

--- interprete.py
+++ interprete.py
@@ -1,3 +1,23 @@
+from tipoToken import tipoToken
+from Token import Token
+import os
+
+#Para mandar los tokens de salida
+estructura=Token('','',None)
+
+#Para identificar una cadena
+def identificador(cad):
+  j=1
+  if cad[0].isalpha():
+    for i in cad:
+      if i.isdigit() or i.isalpha() or i=='_' or i=='-':
+        j+=1
+      else:
+        break
+      print(Token(tipoToken.IDENTIFIER,cad[:j-1],None).toString())
+      estructura.almacenar(tipoToken.IDENTIFIER,cad[:j-1],None)
+      return j-1
+
 #Funcion para manipular el texto desde un archivo 
 def scanerArchivo():
   print('pa\' los archivos')
