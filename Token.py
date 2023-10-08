517c2de57ad11546e1f21b682cfc63c014169ec4
CesarZamudio123 <147220285+CesarZamudio123@users.noreply.github.com>
2023-10-07 22:51:09 -0600

    Token.py

--- Token.py
+++ Token.py
@@ -1,1 +1,29 @@
 
+  class Token():
+    def __init__(self,tipo,lexema):
+      self.tipo=tipo
+      self.lexema=lexema
+      self.literal=None
+      self.salida=[]
+
+    def __init__(self,tipo,lexema,literal):
+      self.tipo=tipo
+      self.literal=lexema
+      self.salida=[]
+
+    def toString(self)
+        if self.literal==None:
+          sal='<'+str(self.tipo)+''+str(self.lexema)+'>'
+        else:
+          sal='<'+str(self.tipo)+''+str(self.lexema)+''+str(self.literal)+'>'
+        return sal
+
+    def almacenar(self,tipo,lexema,literal)
+        entrada=['','','']
+        entrada[0]=tipo
+        entrada[1]=lexema
+        entrada[2]=literal
+        self.salida+=entrada
+
+    def imprimirEstruc(self):
+      print(self.salida)
