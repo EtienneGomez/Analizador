753cac9e7ab88ddf7c48f5ddb3ac0c28a8e84e5b
CesarZamudio123 <147220285+CesarZamudio123@users.noreply.github.com>
2023-10-08 18:04:32 -0600

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,22 +1,22 @@
 from tipoToken import tipoToken
 
-  class Token():
+class Token():
     def __init__(self,tipo,lexema):
-      self.tipo=tipo
-      self.lexema=lexema
-      self.literal=None
-      self.salida=[]
+        self.tipo=tipo
+        self.lexema=lexema
+        self.literal=None
+        self.salida=[]
 
     def __init__(self,tipo,lexema,literal):
-      self.tipo=tipo
-      self.literal=lexema
-      self.salida=[]
+        self.tipo=tipo
+        self.literal=lexema
+        self.salida=[]
 
     def toString(self):
         if self.literal==None:
-          sal='<'+str(self.tipo)+''+str(self.lexema)+'>'
+            sal='<'+str(self.tipo)+''+str(self.lexema)+'>'
         else:
-          sal='<'+str(self.tipo)+''+str(self.lexema)+''+str(self.literal)+'>'
+            sal='<'+str(self.tipo)+''+str(self.lexema)+''+str(self.literal)+'>'
         return sal
 
     def almacenar(self,tipo,lexema,literal):
@@ -27,4 +27,4 @@ from tipoToken import tipoToken
         self.salida+=entrada
 
     def imprimirEstruc(self):
-      print(self.salida)
+        print(self.salida)
